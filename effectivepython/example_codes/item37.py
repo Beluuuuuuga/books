c41fcda61dcb7d18ac4975804786c923aeb755be
kokiinoue <inoue.koki15@gmail.com>
2020-09-08 00:17:34 +0900

    crate ex7

--- effectivepython/example_codes/item37.py
+++ effectivepython/example_codes/item37.py
@@ -122,3 +122,54 @@ a = Grade(95, 0.45)
 b = Grade(85, 0.55)
 average_grade = ((a.score * a.weight) + (b.score * b.weight)) / (a.weight + b.weight)
 print(average_grade)
+
+
+# Example 07
+class Subject:
+    def __init__(self):
+        self._grades = []
+
+    def report_grade(self, score, weight):
+        self._grades.append(Grade(score, weight))
+
+    def average_grade(self):
+        total, total_weight = 0, 0
+        for grade in self._grades:
+            total += grade.score * grade.weight
+            total_weight += grade.weight
+        return total / total_weight
+
+
+class Student:
+    def __init__(self):
+        self._subjects = defaultdict(Subject)
+    
+    def get_subject(self, name):
+        return self._subjects[name]
+
+    def average_grade(self):
+        total, count = 0, 0
+        for subject in self._subjects.values():
+            total += subject.average_grade()
+            count += 1
+        return total / count
+
+
+class Gradebook:
+    def __init__(self):
+        self._students = defaultdict(Student)
+    
+    def get_student(self, name):
+        return self._students[name]
+
+
+book = Gradebook()
+albert = book.get_student('Albert Einstein')
+math = albert.get_subject('Math')
+math.report_grade(75, 0.05)
+math.report_grade(65, 0.15)
+math.report_grade(70, 0.80)
+gym = albert.get_subject('Gym')
+gym.report_grade(100, 0.40)
+gym.report_grade(85, 0.60)
+print(albert.average_grade())
