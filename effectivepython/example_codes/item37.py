d6d736adc5756c8b4b2d813800b719ab5f499131
kokiinoue <inoue.koki15@gmail.com>
2020-09-07 21:52:04 +0900

    crate ex5

--- effectivepython/example_codes/item37.py
+++ effectivepython/example_codes/item37.py
@@ -102,3 +102,13 @@ total = sum(score * weight for score, weight in grades)
 total_weight = sum(weight for _, weight in grades)
 average_grade = total / total_weight
 print(average_grade)
+
+
+# Example 05
+grades = []
+grades.append((95, 0.45, 'Great job'))
+grades.append((85, 0.55, 'Better next time'))
+total = sum(score * weight for score, weight, _ in grades)
+total_weight = sum(weight for _, weight, _ in grades)
+average_grade = total / total_weight
+print(average_grade)
