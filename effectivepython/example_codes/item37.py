a74a2205788114d5bff75d82f73e968a2514b6f9
kokiinoue <inoue.koki15@gmail.com>
2020-09-07 20:27:00 +0900

    crate ex2

--- effectivepython/example_codes/item37.py
+++ effectivepython/example_codes/item37.py
@@ -13,20 +13,43 @@ class SimpleGradebook:
         grades = self._grades[name]
         return sum(grades) / len(grades)
 
+
 book = SimpleGradebook()
 book.add_student('Isaac Newton')
 book.report_grade('Isaac Newton', 90)
 book.report_grade('Isaac Newton', 95)
 book.report_grade('Isaac Newton', 85)
-
 print(book.average_grade('Isaac Newton'))
 
-from collections import defaultdict
 
 # Example 02
+from collections import defaultdict
+
 class BySubjectGradebook:
     def __init__(self):
         self._grades = {} # 外側のリスト
     
     def add_student(self, name):
         self._grades[name] = defaultdict(list) # 内側のリスト
+
+    def report_grade(self, name, subject, grade):
+        by_subject = self._grades[name]
+        grade_list = by_subject[subject]
+        grade_list.append(grade)
+
+    def average_grade(self, name):
+        by_subject = self._grades[name]
+        total, count = 0, 0
+        for grades in by_subject.values():
+            total += sum(grades)
+            count += len(grades)
+        return total / count 
+
+
+book = BySubjectGradebook()
+book.add_student('Albert Einstein')
+book.report_grade('Albert Einstein', 'Math', 75)
+book.report_grade('Albert Einstein', 'Math', 65)
+book.report_grade('Albert Einstein', 'Gym', 90)
+book.report_grade('Albert Einstein', 'Gym', 95)
+print(book.average_grade('Albert Einstein'))
