7e59ed35471d82a748295d93a7c353844ef81c35
Koki Inoue <kokiinoue@KokinoMacBook-Pro.local>
2020-09-08 19:24:28 +0900

    create ex3

--- effectivepython/example_codes/item38.py
+++ effectivepython/example_codes/item38.py
@@ -3,6 +3,7 @@ names = ['Scarates', 'Plato', 'Aristotle']
 names.sort(key=len)
 print(names)
 
+
 # Example 02
 def log_missing():
     print('Key added')
@@ -21,3 +22,23 @@ print('Before', dict(result))
 for key, amount in increments:
     result[key] += amount
 print('After', dict(result))
+
+
+# Example 03
+def increment_with_report(current, increments):
+    added_count = 0
+
+    def missing():
+        nonlocal added_count
+        added_count += 1
+        return 0
+    
+    result = defaultdict(missing, current)
+    for key, amount in increments:
+        result[key] += amount
+
+    return result, added_count
+
+result, count = increment_with_report(current, increments)
+print(dict(result))
+assert count == 2
