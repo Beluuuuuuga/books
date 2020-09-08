ac5ef5454be29162f70c4beabf538b72cdd921db
Koki Inoue <kokiinoue@KokinoMacBook-Pro.local>
2020-09-08 20:11:08 +0900

    create ex3

--- effectivepython/example_codes/item39.py
+++ effectivepython/example_codes/item39.py
@@ -13,3 +13,16 @@ class PathInputData(InputData):
     def read(self):
         with open(self.path) as f:
             return f.read()
+
+
+# Example 03
+class Worker:
+    def __init__(self, input_data):
+        self.input_data = input_data
+        self.result = None
+    
+    def map(self):
+        raise NotImplementedError
+
+    def reduce(self, other):
+        raise NotImplementedError
