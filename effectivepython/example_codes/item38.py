838783dec1423decc9aae24db965769a8d0ae65e
Koki Inoue <kokiinoue@KokinoMacBook-Pro.local>
2020-09-08 19:14:06 +0900

    create ex2

--- effectivepython/example_codes/item38.py
+++ effectivepython/example_codes/item38.py
@@ -2,3 +2,22 @@
 names = ['Scarates', 'Plato', 'Aristotle']
 names.sort(key=len)
 print(names)
+
+# Example 02
+def log_missing():
+    print('Key added')
+    return 100
+
+from collections import defaultdict
+
+current = {'green': 12, 'blue': 3}
+increments = [
+    ('red', 5),
+    ('blue', 17),
+    ('orange', 9),
+]
+result = defaultdict(log_missing, current)
+print('Before', dict(result))
+for key, amount in increments:
+    result[key] += amount
+print('After', dict(result))
