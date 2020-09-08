f192ebb299f8b7e558a096b5965119dbbf17afa4
Koki Inoue <kokiinoue@KokinoMacBook-Pro.local>
2020-09-08 20:06:12 +0900

    create ex2

--- effectivepython/example_codes/item39.py
+++ effectivepython/example_codes/item39.py
@@ -2,3 +2,14 @@
 class InputData:
     def read(self):
         raise NotImplementedError
+
+
+# Example 02
+class PathInputData(InputData):
+    def __init__(self, path):
+        super().__init__()
+        self.path = path
+    
+    def read(self):
+        with open(self.path) as f:
+            return f.read()
